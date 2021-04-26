955532a14b1ecaeaa480a9feafe5a72320ef8471
Galactuuus <galactuuus@gmail.com>
2021-04-26 17:32:09 +0200

    Rename

--- middleware/checkJwt.js
+++ middleware/checkJwt.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 export const checkJwt = (req, res, next) => {
     try {
-    jwt.verify(req.headers.authentication, process.env.SECRET);
+    jwt.verify(req.headers.auth, process.env.SECRET);
     next();
     } catch(e) {
         res.sendStatus(401)
